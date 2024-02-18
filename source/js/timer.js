7200cba9f980ce68c76c32bea933940be6225a3d
Fenn <nicolaileff@icloud.com>
2024-02-17 19:36:37 -0600

    Update timer.js

--- source/js/timer.js
+++ source/js/timer.js
@@ -1,1 +1,78 @@
-
+let startBtn = document.getElementById('start'); 
+let stopBtn = document.getElementById('stop'); 
+let resetBtn = document.getElementById('reset'); 
+  
+let millisecond = 00; 
+let minute = 00; 
+let second = 00; 
+let count = 00; 
+  
+startBtn.addEventListener('click', function () { 
+    timer = true; 
+    stopWatch(); 
+}); 
+  
+stopBtn.addEventListener('click', function () { 
+    timer = false; 
+}); 
+  
+resetBtn.addEventListener('click', function () { 
+    timer = false; 
+    minute = 0; 
+    second = 0; 
+    millisecond = 0; 
+    count = 0; 
+    document.getElementById('min').innerHTML = "00"; 
+    document.getElementById('sec').innerHTML = "00"; 
+    document.getElementById('mil').innerHTML = "00"; 
+    document.getElementById('count').innerHTML = "00"; 
+}); 
+  
+function stopWatch() { 
+    if (timer) { 
+        count++; 
+  
+        if (count == 100) { 
+            second++; 
+            count = 0; 
+        } 
+  
+        if (millisecond == 1000) { 
+            second++; 
+            second = 0; 
+        } 
+  
+        if (second == 60) { 
+            minute++; 
+            minute = 0; 
+            second = 0; 
+        } 
+  
+        let milString = millisecond; 
+        let minString = minute; 
+        let secString = second; 
+        let countString = count; 
+  
+        if (millisecond < 10) { 
+            milString = "0" + milString; 
+        } 
+  
+        if (minute < 10) { 
+            minString = "0" + minString; 
+        } 
+  
+        if (second < 10) { 
+            secString = "0" + secString; 
+        } 
+  
+        if (count < 10) { 
+            countString = "0" + countString; 
+        } 
+  
+        document.getElementById('min').innerHTML = minString; 
+        document.getElementById('sec').innerHTML = secString; 
+        document.getElementById('mil').innerHTML = milString; 
+        document.getElementById('count').innerHTML = countString; 
+        setTimeout(stopWatch, 10); 
+    } 
+}
