1c742bc9b969a7414ab28ad95a2107a0198a7723
Fenn <nicolaileff@icloud.com>
2024-02-18 08:01:19 -0600

    Update scramble.js

--- source/js/scramble.js
+++ source/js/scramble.js
@@ -2,7 +2,7 @@ var scramble = ""
 var Possible = ["F", "R", "U", "B", "L", "D"]
 var Modifiers = ["", "'", "2"]
 var i = 1
-while (i != 20);
+while (i < 20);
   var scramble = scramble + " " + Possible[Math.floor(Math.random() * Possible.length)] + Modifiers[Math.floor(Math.random() * Modifiers.length)]
   var i = i + 1
 console.log(scramble)
