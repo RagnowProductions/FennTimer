7029f7dd7f283b256a1771cdad991581e0e57a3b
Fenn <nicolaileff@icloud.com>
2024-02-20 17:00:48 -0600

    Update timer.js
    forgot that the count variable was milliseconds

--- source/js/timer.js
+++ source/js/timer.js
@@ -1,78 +1,78 @@
 let startBtn = document.getElementById('start'); 
 let stopBtn = document.getElementById('stop'); 
 let resetBtn = document.getElementById('reset'); 
-  
-let millisecond = 00; 
+
+let hour = 00; 
 let minute = 00; 
 let second = 00; 
 let count = 00; 
-  
+
 startBtn.addEventListener('click', function () { 
-    timer = true; 
-    stopWatch(); 
+	timer = true; 
+	stopWatch(); 
 }); 
-  
+
 stopBtn.addEventListener('click', function () { 
-    timer = false; 
+	timer = false; 
 }); 
-  
+
 resetBtn.addEventListener('click', function () { 
-    timer = false; 
-    minute = 0; 
-    second = 0; 
-    millisecond = 0; 
-    count = 0; 
-    document.getElementById('min').innerHTML = "00"; 
-    document.getElementById('sec').innerHTML = "00"; 
-    document.getElementById('mil').innerHTML = "00"; 
-    document.getElementById('count').innerHTML = "00"; 
+	timer = false; 
+	hour = 0; 
+	minute = 0; 
+	second = 0; 
+	count = 0; 
+	document.getElementById('hr').innerHTML = "00"; 
+	document.getElementById('min').innerHTML = "00"; 
+	document.getElementById('sec').innerHTML = "00"; 
+	document.getElementById('count').innerHTML = "00"; 
 }); 
-  
+
 function stopWatch() { 
-    if (timer) { 
-        count++; 
-  
-        if (count == 100) { 
-            second++; 
-            count = 0; 
-        } 
-  
-        if (millisecond == 1000) { 
-            second++; 
-            second = 0; 
-        } 
-  
-        if (second == 60) { 
-            minute++; 
-            minute = 0; 
-            second = 0; 
-        } 
-  
-        let milString = millisecond; 
-        let minString = minute; 
-        let secString = second; 
-        let countString = count; 
-  
-        if (millisecond < 10) { 
-            milString = "0" + milString; 
-        } 
-  
-        if (minute < 10) { 
-            minString = "0" + minString; 
-        } 
-  
-        if (second < 10) { 
-            secString = "0" + secString; 
-        } 
-  
-        if (count < 10) { 
-            countString = "0" + countString; 
-        } 
-  
-        document.getElementById('min').innerHTML = minString; 
-        document.getElementById('sec').innerHTML = secString; 
-        document.getElementById('mil').innerHTML = milString; 
-        document.getElementById('count').innerHTML = countString; 
-        setTimeout(stopWatch, 10); 
-    } 
+	if (timer) { 
+		count++; 
+
+		if (count == 100) { 
+			second++; 
+			count = 0; 
+		} 
+
+		if (second == 60) { 
+			minute++; 
+			second = 0; 
+		} 
+
+		if (minute == 60) { 
+			hour++; 
+			minute = 0; 
+			second = 0; 
+		} 
+
+		let hrString = hour; 
+		let minString = minute; 
+		let secString = second; 
+		let countString = count; 
+
+		if (hour < 10) { 
+			hrString = "0" + hrString; 
+		} 
+
+		if (minute < 10) { 
+			minString = "0" + minString; 
+		} 
+
+		if (second < 10) { 
+			secString = "0" + secString; 
+		} 
+
+		if (count < 10) { 
+			countString = "0" + countString; 
+		} 
+
+		document.getElementById('hr').innerHTML = hrString; 
+		document.getElementById('min').innerHTML = minString; 
+		document.getElementById('sec').innerHTML = secString; 
+		document.getElementById('count').innerHTML = countString; 
+		setTimeout(stopWatch, 10); 
+	} 
 }
